bf063ac7774b44c619b31b45ebc1af44bf62743d
lefes <lefes123@gmail.com>
2022-12-10 21:47:49 +0300

    Added compute resources and export to CSV

--- main.go
+++ main.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/csv"
 	"os"
+	"strconv"
 
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/compute/v1"
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/resourcemanager/v1"
 	ycsdk "github.com/yandex-cloud/go-sdk"
 	"gopkg.in/yaml.v2"
 )
-
-type Request struct {
-	FolderId string `json:"folderId"`
-	Tag      string `json:"tag"`
-}
 
 type Response struct {
 	StatusCode int         `json:"statusCode"`
@@ -29,6 +25,21 @@ type YandexCreds struct {
 	} `yaml:"profiles"`
 }
 
+type Instance struct {
+	FolderName string
+	Name       string
+	CPU        int
+	Memory     int
+	Fraction   int
+	Disks      []Disk
+}
+
+type Disk struct {
+	Name string
+	Size int
+}
+
+// TODO: Add support of take token from env and pass it as argument
 func getToken() string {
 	var creds YandexCreds
 	// Reading token from yaml file
@@ -44,34 +55,9 @@ func getToken() string {
 	return creds.Profiles.Default.Token
 }
 
-func StartComputeInstances(ctx context.Context, request *Request) (*Response, error) {
-	// Reading token from yaml file
-	token := getToken()
-	sdk, err := ycsdk.Build(ctx, ycsdk.Config{
-		Credentials: ycsdk.OAuthToken(token),
-	})
-	if err != nil {
-		return nil, err
-	}
-	listInstancesResponse, err := sdk.Compute().Instance().List(ctx, &compute.ListInstancesRequest{
-		FolderId: request.FolderId,
-	})
-	if err != nil {
-		return nil, err
-	}
-	instances := listInstancesResponse.GetInstances()
-	count := 0
-	for _, i := range instances {
-		fmt.Printf("Starting instance %s", i.Name)
-		count++
-	}
-	return &Response{
-		StatusCode: 200,
-		Body:       fmt.Sprintf("Started %d instances", count),
-	}, nil
-}
-
-func getFoldersList(ctx context.Context) ([]string, error) {
+// TODO: Add pagination support
+// TODO: Add support to muiltiple clouds
+func getFoldersList(ctx context.Context) ([]*resourcemanager.Folder, error) {
 	token := getToken()
 	sdk, err := ycsdk.Build(ctx, ycsdk.Config{
 		Credentials: ycsdk.OAuthToken(token),
@@ -83,30 +69,108 @@ func getFoldersList(ctx context.Context) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	var folders []string
+	folders := make([]*resourcemanager.Folder, 0)
 	for _, cloud := range clouds.Clouds {
 		cloudFolders, err := sdk.ResourceManager().Folder().List(ctx, &resourcemanager.ListFoldersRequest{CloudId: cloud.Id})
 		if err != nil {
 			return nil, err
 		}
-		for _, folder := range cloudFolders.Folders {
-			folders = append(folders, folder.Name)
-		}
+		folders = append(folders, cloudFolders.Folders...)
 	}
 	return folders, nil
 }
 
+// TODO: Add pagination support
+// TODO: Add goroutines with throttling and dynamic number of goroutines with default value
+func getComputeResources(ctx context.Context, folders []*resourcemanager.Folder) (map[string][]Instance, error) {
+	token := getToken()
+	sdk, err := ycsdk.Build(ctx, ycsdk.Config{
+		Credentials: ycsdk.OAuthToken(token),
+	})
+	if err != nil {
+		return nil, err
+	}
+	instances := make(map[string][]Instance)
+	for _, folder := range folders {
+		computeResources, err := sdk.Compute().Instance().List(ctx, &compute.ListInstancesRequest{FolderId: folder.Id})
+		if err != nil {
+			return nil, err
+		}
+		for _, computeResource := range computeResources.Instances {
+			instance := Instance{
+				FolderName: folder.Name,
+				Name:       computeResource.Name,
+				CPU:        int(computeResource.Resources.Cores),
+				Memory:     int(computeResource.Resources.Memory),
+				Fraction:   int(computeResource.Resources.CoreFraction),
+			}
+			// Getting boot disk size
+			bootDisk, err := sdk.Compute().Disk().Get(ctx, &compute.GetDiskRequest{DiskId: computeResource.BootDisk.DiskId})
+			if err != nil {
+				return nil, err
+			}
+			instance.Disks = append(instance.Disks, Disk{
+				Name: bootDisk.Name,
+				Size: int(bootDisk.Size),
+			})
+			// Getting Secondary disks size
+			for _, disk := range computeResource.SecondaryDisks {
+				secondaryDisk, err := sdk.Compute().Disk().Get(ctx, &compute.GetDiskRequest{DiskId: disk.DiskId})
+				if err != nil {
+					return nil, err
+				}
+				instance.Disks = append(instance.Disks, Disk{
+					Name: secondaryDisk.Name,
+					Size: int(secondaryDisk.Size),
+				})
+			}
+
+			instances[folder.Id] = append(instances[folder.Id], instance)
+		}
+	}
+	return instances, nil
+}
+
+// TODO: get out calculation of resources to separate function
+// TODO: Add option to set output file name
+func exportToCSV(resources map[string][]Instance) {
+	f, err := os.Create("instances.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	w := csv.NewWriter(f)
+	defer w.Flush()
+
+	for _, folder := range resources {
+		cpus := 0
+		memory := 0
+		disc := 0
+		for _, instance := range folder {
+			cpus += instance.CPU * (instance.Fraction / 100)
+			memory += instance.Memory / (1 << 30)
+			for _, disk := range instance.Disks {
+				disc += disk.Size / (1 << 30)
+			}
+		}
+		err := w.Write([]string{folder[0].FolderName, "CPU - " + strconv.Itoa(cpus) + " шт\n" + "RAM - " + strconv.Itoa(memory) + " гб\n" + "Disk - " + strconv.Itoa(disc) + " гб\n"})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
+
 func main() {
 	ctx := context.Background()
-	request := &Request{
-		FolderId: "b1g9memkicue053heorv",
-		Tag:      "test",
-	}
-	response, err := StartComputeInstances(ctx, request)
+	folders, err := getFoldersList(ctx)
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
-	fmt.Println(response)
-	fmt.Println(getFoldersList(ctx))
+	computeResources, err := getComputeResources(ctx, folders)
+	if err != nil {
+		panic(err)
+	}
+	exportToCSV(computeResources)
+
 }
